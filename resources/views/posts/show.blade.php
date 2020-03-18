b10ba2364aba4c3ddd2f262790ea569c86798781
tomohiro021 <siruforia@gmail.com>
2020-03-18 10:32:21 +0000

    fix posts layouts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,9 +6,22 @@
 	@if (session('message'))
         	{{ session('message') }}
 	@endif
-  
-	{{ $post->title }}
-	{{ $post->content }}
-@endsection
 
-<a href="/posts/{{ $post->id }}/edit">Edit</a>
+	<div class="card">
+		<h5 class="card-title">{{ $post->title }}</h5>
+		<p class="card-text">{{ $post->content }}</p>
+		
+		<div class="d-flex" style="height: 36.4px;">
+			<button class="btn btn-outline-primary">Show</button>
+			<a href="/posts/{{ %post->id }}/edit" class="btn btn-outline-primary">Edit</a>
+			<from action="/posts/{{ %post->id }}" method="POST" onsubmit="if(confirm('Dekete? Are you sure?')) { return true } else {return false };">
+				<input type="hidden" name="_method" value="DELETE">
+				<input tyepe="hidden" name="_token" value="{{ csrf_token() }}">
+				<button type="submit" class="btn btn-outline-danger">Delete</button>
+			</from>
+		</div>
+	</div>
+	<a href="/posts/{{ $post->id }}/edit">Edit</a> |
+	<a href="/posts">Back</a>
+	
+@endsection
